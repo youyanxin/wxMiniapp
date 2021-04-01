6cbe68e6f984bb5e5c8472e784372de00277be58
bigod589 <544247910@qq.com>
2021-04-01 18:58:07 +0800

    测试提交
    测试提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '测试提交',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
